e97b2d0027f109d4f81c7f4919ed2bbfe035e507
thomasbrasil46 <thomasbrasilpb@gmail.com>
2021-11-26 01:05:24 -0300

    :package: menu sidebar

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", function(){
-    document.querySelector(".sidebar").classList.toggle("show-menu");
-});
+document.querySelector(".hamburger").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
